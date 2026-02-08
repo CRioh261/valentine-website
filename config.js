27ed2192236af515c513b2e1abc3e82c1aecb634
CRioh261 <cedricandrianirina8@gmail.com>
2026-02-08 16:52:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/drlphawbg/video/upload/v1770550070/Tinahime_-_Maybe_Its_Too_Soon_Lyrics_Video_pzuvxx.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/drlphawbg/video/upload/v1770558612/New_West_-_Those_Eyes_Lyrics_suap38.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
